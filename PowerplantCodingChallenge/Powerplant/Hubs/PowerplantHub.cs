bcb8e0e2b96c6311ad8ae56d597622e3574f1f13
Samad Hatsijev <s.hatsijev@hotmail.com>
2022-02-09 22:26:19 +0100

    PowerplantHub Documentation

--- PowerplantCodingChallenge/Powerplant/Hubs/PowerplantHub.cs
+++ PowerplantCodingChallenge/Powerplant/Hubs/PowerplantHub.cs
@@ -5,6 +5,11 @@ namespace Powerplant.Hubs
 {
     public class PowerplantHub : Hub
     {
+        /// <summary>
+        /// Receive the list of results and send back to all connected clients.
+        /// </summary>
+        /// <param name="result">List of results</param>
+        /// <returns>Return list of results to all connected clients.</returns>
         public Task SendResult(IEnumerable<ResultModel> result)
         {
             return Clients.All.SendAsync("ReceiveResult", result);
