1b2689ba8ba47b6f694a7402f207f645d6f7b870
Samad Hatsijev <s.hatsijev@hotmail.com>
2022-02-11 10:25:32 +0100

    Last update
    (the algo speaks for itself, very easy to understand)

--- PowerplantCodingChallenge/CodingChallenge/Services/PowerplantService.cs
+++ PowerplantCodingChallenge/CodingChallenge/Services/PowerplantService.cs
@@ -18,10 +18,7 @@ namespace CodingChallenge.Services
 
                 Powerplant nextPowerPlant = index + 1 >= powerPlants.Count ? null : powerPlants[index + 1];
 
-                Result result = new Result()
-                {
-                    Name = powerPlant.Name
-                };
+                int power = 0;
 
                 double pMinOutput = powerPlant.GetMinPower(payload.Fuels);
 
@@ -29,57 +26,61 @@ namespace CodingChallenge.Services
 
                 double nextPMinOutput = nextPowerPlant is null ? 0 : nextPowerPlant.GetMinPower(payload.Fuels);
 
-                if (leftLoad > pMaxOutput && (leftLoad - pMaxOutput) < nextPMinOutput)
+                double leftLoadAfterSubstraction = leftLoad - pMaxOutput;
+
+                if (leftLoad > pMaxOutput && leftLoadAfterSubstraction < nextPMinOutput)
                 {
                     if (powerPlant.IsWindTurbine())
                     {
-                        int leftLoadAfterSubstraction = Convert.ToInt32(leftLoad - pMaxOutput);
-
-                        bool listContainsLeftLoadAfterSubstraction = powerPlants.Any(powerplant => leftLoadAfterSubstraction >= powerplant.PMin && leftLoadAfterSubstraction <= powerplant.PMax && !powerplant.IsWindTurbine());
+                        bool listContainsLeftLoadAfterSubstraction = powerPlants.Any(powerplant => (int)leftLoadAfterSubstraction >= powerplant.PMin && (int)leftLoadAfterSubstraction <= powerplant.PMax && !powerplant.IsWindTurbine());
 
                         bool listContainsLeftLoad = powerPlants.Any(powerplant => leftLoad >= powerplant.PMin && leftLoad <= powerplant.PMax && !powerplant.IsWindTurbine());
 
                         if (listContainsLeftLoadAfterSubstraction && !listContainsLeftLoad)
                         {
-                            result.P = (int)Math.Round(pMaxOutput);
+                            power = (int)Math.Round(pMaxOutput);
 
                             leftLoad -= (int)Math.Round(pMaxOutput);
                         }
                         else
                         {
-                            result.P = 0;
+                            power = 0;
                         }
                     }
                     else
                     {
                         double temp = leftLoad - nextPMinOutput;
 
-                        result.P = (int)Math.Round(temp);
+                        power = (int)Math.Round(temp);
 
                         leftLoad -= temp;
                     }
                 }
                 else if (leftLoad >= pMinOutput)
                 {
-                    if (leftLoad - pMaxOutput > 0)
+                    if (leftLoadAfterSubstraction > 0)
                     {
-                        result.P = (int)Math.Round(pMaxOutput);
+                        power = (int)Math.Round(pMaxOutput);
 
                         leftLoad -= pMaxOutput;
                     }
                     else
                     {
-                        result.P = (int)Math.Round(leftLoad);
+                        power = (int)Math.Round(leftLoad);
 
                         leftLoad -= leftLoad;
                     }
                 }
                 else
                 {
-                    result.P = 0;
+                    power = 0;
                 }
 
-                output.Add(result);
+                output.Add(new Result()
+                {
+                    Name = powerPlant.Name,
+                    P = power
+                });
             }
 
             return output;
